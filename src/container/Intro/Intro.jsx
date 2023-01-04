e679c4b946993b687a00fb8458169aa7b799d343
anisa707 <anisa_hajrullaj@yahoo.com>
2023-01-04 22:10:39 +0100

    Laurels and Gallery Sections Done 2: 40

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -7,6 +7,14 @@ import './Intro.css';
 const Intro = () => {
   const [playVideo, setPlayVideo] = React.useState(false);
   const vidRef = React.useRef();
+  const handleVideo = () => {
+    setPlayVideo((previousPlayVideo) => !previousPlayVideo);
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  }
   return (
     <div className='app__video'>
       <video
@@ -19,8 +27,15 @@ const Intro = () => {
       />
 
       <div className='app__video-overlay flex__center'>
-        <div className='app__video-overlay_circle flex__center'>
-
+        <div 
+        className='app__video-overlay_circle flex__center'
+        onClick={handleVideo}
+        >
+          {
+            playVideo 
+            ? <BsPauseFill color='#fff' fontSize={30} />
+            : <BsFillPlayFill color='#fff' fontSize={30} />
+          }
         </div>
 
       </div>
